055bc3123889dac3f51fa575ab3fcfa3c5d243d8
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2023-04-05 15:29:32 +0530

    frontend

---
+++ client/src/pages/Account.tsx
@@ -0,0 +1,23 @@
+import Navbar from "../components/Navbar";
+
+type Props = {}
+
+const Account = (props: Props) => {
+    return (
+        <div className='bg-[#222831] min-h-[100vh]'>
+            <Navbar />
+            <h1 className="ml-10 py-2 mt-[100px] text-4xl font-bold text-[#eee]">Hello <span className="text-[#00ADB5]">User!</span></h1>
+            <div className="flex bg-[#283035] rounded-xl my-4 max-w-[950px] p-4 flex-col-reverse sm:flex-row md:max-w-[850px] md:min-w-[845px] justify-between mx-auto items-center">
+                <div className="flex flex-col justify-between">
+                    <p className="text-xl sm:text-4xl py-2 text-[#57676f] font-bold">nencncncc cnccncnecn j3cbi3rncr3cnocn</p>
+                    <p className="text-sm font-semibold my-3 sm:text-sm text-[#57676f]">cnriocv3nrvrvbrj jrv rjbvrvbrjv rvjrvbrvbrw3vbw3v</p>
+                    <div className="flex mt-2 cursor-pointer">orncjr3bjr3bcvr3jvb</div>
+                    <p className="text-[#57676f] mt-8 font-bold text-[12px]">kjncjkr3ncjr3c</p>
+                </div>
+                <img className="object-contain rounded-xl sm:max-w-[250px]" src="/login.png"/>
+            </div>
+        </div>
+    )
+}
+
+export default Account
--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -10,18 +10,22 @@ const Blog = (props: Props) => {
             <Navbar />
             <div className="mt-[90px] mb-10 ml-[5vw] sm:ml-[10vw]">
                 <img className="w-[90vw] h-[35vh] rounded-2xl sm:w-[80vw] sm:h-[45vh]" src="login.png" alt="" />
-                <h1 className="mt-4 text-4xl py-4 font-bold text-[#00ADB5]">This is a blog</h1>
+                {!updatemode && <h1 className="mt-4 text-4xl py-4 font-bold text-[#00ADB5]">This is a blog</h1>}
+                {updatemode && <input className='mx-4 mb-3 bg-[#222831] rounded-md placeholder-[#424f58] text-[#424f58] outline-none w-[300px] sm:w-[450px] pl-4 h-[40px] font-bold' placeholder='My first blog.....'/>}
                 <div className="flex justify-between items-center">
                     <div>
                         <p className="mt-4 text-lg font-bold text-[#bababa]">Author: <span className="text-[#00ADB5]">Omkarsk82</span></p>
-                        <p className="mt-4 text-lg font-bold text-[#bababa]">Tags: <span className=" bg-[#00ADB5] px-3 rounded-3xl text-[#222831]">AI</span></p>
+                        {!updatemode && <p className="mt-4 text-lg font-bold text-[#bababa]">Tags: <span className=" bg-[#00ADB5] px-3 rounded-3xl text-[#222831]">AI</span></p>}
+                        {updatemode && <input className='mx-4 mb-3 bg-[#222831] rounded-md placeholder-[#424f58] text-[#424f58] outline-none w-[300px] sm:w-[450px] pl-4 h-[40px] font-bold' placeholder='My first blog.....'/>}
                     </div>
                     <div className="mt-[-30px] sm:mr-[10vw]">
-                        <button className="px-4 py-1 m-2 rounded-xl bg-[#2e373d] text-[#00ADB5] cursor-pointer mr-2">Update</button>
+                        {!updatemode &&<button onClick={() => {setUpadatemode(true)}} className="px-4 py-1 m-2 rounded-xl bg-[#2e373d] text-[#00ADB5] cursor-pointer mr-2">Update</button>}
+                        {updatemode && <button onClick={() => {setUpadatemode(false)}} className="px-4 py-1 m-2 rounded-xl bg-[#2e373d] text-[#00ADB5] cursor-pointer mr-2">Publish</button>}
                         <button className="px-4 py-1 rounded-xl bg-[#2e373d] text-[#00ADB5] cursor-pointer">Delete</button>
                     </div>
                 </div>
-                <p className="mt-6 text-justify font-bold text-[#4f5e68] sm:mr-[10vw]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                {!updatemode && <p className="mt-6 text-justify font-bold text-[#4f5e68] sm:mr-[10vw]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>}
+                {updatemode && <input className='mx-4 mb-3 bg-[#222831] rounded-md placeholder-[#424f58] text-[#424f58] outline-none w-[300px] sm:w-[450px] pl-4 h-[40px] font-bold' placeholder='My first blog.....'/>}
             </div>
         </div>
     ) 
