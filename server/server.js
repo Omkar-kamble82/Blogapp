bb742a0e144f1e696f31669ecf6405bc4e9fa755
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2023-04-07 09:29:27 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
+const blogRoutes  = require('./routes/blogs')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
 app.use(express.json())
+const allowedOrigins = [
+    process.env.CORS_ORIGIN,
+]
+
+app.use(
+    cors({
+        origin: allowedOrigins
+    })
+)
+
+app.use('/api/blogs', blogRoutes)
+app.use('/api/user', userRoutes)
 
 mongoose.set('strictQuery', true).connect(process.env.MONGO_URI)
     .then(() => {
