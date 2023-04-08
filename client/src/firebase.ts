8ac631a2a963a1e5034931ca3513cd41abc4dc0f
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2023-04-08 19:38:18 +0530

    final

---
+++ client/src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.API_KEY,
+    authDomain:  import.meta.env.AUTH_DOMAIN,
+    projectId:  import.meta.env.PROJECT_ID,
+    storageBucket:  import.meta.env.STORAGE_BUCKET,
+    messagingSenderId:  import.meta.env.MESSAGE_SENDER_ID,
+    appId: import.meta.env.APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
