544dfd5b393f0122258b3961a2b37eacae54252f
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2023-04-09 10:58:21 +0530

    final

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -37,33 +37,33 @@ const Login = () => {
             {register && <div className='flex mx-4 shadow-lg justify-center rounded-xl items-center max-w-[500px] flex-col my-10 overflow-hidden'>
                 <div className="relative w-full h-[170px]">
                     <img className='w-full object-fill h-[170px]' src="/login.png" alt="login-img" />
-                    <div className="absolute inset-0 bg-black/60 flex justify-center items-center"><img className="h-20 w-50" src="/logo.png" alt="logo" /></div>
+                    <div className="absolute inset-0 bg-black/60 flex justify-center items-center"><img className="h-15 w-44 sm:h-20 sm:w-60" src="/logo.png" alt="logo" /></div>
                 </div>
                 <div className='w-full bg-[#283035] flex justify-center flex-col'>
                     <h1 className='mt-4 ml-5 text-[#00ADB5] text-[27px] pb-6 font-bold'>Sign In</h1>
                     <form onSubmit={submitlogin}>
-                        <input onChange={(e) => {setUsername(e.target.value)}} className='mx-4 bg-[#222831] rounded-md placeholder-[#424f58] text-[#424f58] outline-none w-[300px] sm:w-[450px] pl-4 h-[40px]' placeholder='Username'/>
-                        <div className="flex bg-[#222831] items-center m-4 mt-5 rounded-md w-[300px] sm:w-[450px] pl-4 h-[40px] justify-between pr-4"><input type={visible ? "text" : "password"} onChange={(e) => {setPassword(e.target.value)}} className='w-[250px] sm:w-[400px] outline-none placeholder-[#424f58] bg-[#222831] text-[#424f58]' placeholder='Password'/><div className="cursor-pointer" onClick={() => {setVisible(!visible)}}><img className="h-6" src="/eye.svg" alt="change_visiblity" /></div></div>
-                        <button type="submit" className="w-[300px] pl-4 h-[40px] mx-4 mt-5 rounded-md text-[#283035] font-bold bg-[#00ADB5] hover:opacity-70 duration-700 sm:w-[450px]">Sign In</button>
+                        <input onChange={(e) => {setUsername(e.target.value)}} className='mx-4 bg-[#222831] rounded-md placeholder-[#424f58] text-[#424f58] outline-none w-[87%] sm:w-[450px] pl-4 h-[40px]' placeholder='Username'/>
+                        <div className="flex bg-[#222831] items-center m-4 mt-5 rounded-md w-[87%] sm:w-[450px] pl-4 h-[40px] justify-between pr-4"><input type={visible ? "text" : "password"} onChange={(e) => {setPassword(e.target.value)}} className='w-[87%] sm:w-[400px] outline-none placeholder-[#424f58] bg-[#222831] text-[#424f58]' placeholder='Password'/><div className="cursor-pointer" onClick={() => {setVisible(!visible)}}><img className="h-6" src="/eye.svg" alt="change_visiblity" /></div></div>
+                        <button type="submit" className="w-[87%] h-[40px] mx-4 mt-5 rounded-md text-[#283035] font-bold bg-[#00ADB5] hover:opacity-70 duration-700 sm:w-[450px]">Sign In</button>
                     </form>
                     {loginerror && <p className="text-[#c72931] font-bold text-center text-sm">{loginerror}</p> }           
                     <p  className="text-center text-teal-50 text-[12px] py-5">Not a member? <span onClick={() => {setRegister(false)}} className="underline cursor-pointer text-[#00ADB5]">Register now</span></p>
                 </div>
             </div>}
             {!register && 
-            <div className='flex mx-4 shadow-lg justify-center rounded-xl items-center max-w-[500px] flex-col min-h-[70vh] my-10 overflow-hidden'>
+            <div className='flex mx-4 shadow-lg justify-center rounded-xl items-center max-w-[500px] flex-col my-10 overflow-hidden'>
                 {isLoading && <div className="absolute inset-0 bg-black/70 z-[11] flex justify-center items-center flex-col"><img className="h-[100px]" src="/loading.gif" alt="" /><p className="text-[#00ADB5] text-[14px]">*If it takes longer, please reload the page and try again.</p></div>}
                 <div className="relative w-full h-[170px]">
                     <img className='w-full object-fill h-[170px]' src="/login.png" alt="login-img" />
-                    <div className="absolute inset-0 bg-black/60 flex justify-center items-center"><img className="h-20 w-50" src="/logo.png" alt="logo" /></div>
+                    <div className="absolute inset-0 bg-black/60 flex justify-center items-center"><img className="h-15 w-44 sm:h-20 sm:w-60" src="/logo.png" alt="logo" /></div>
                 </div>
                 <div className='w-full bg-[#283035] flex justify-center flex-col'>
                     <h1 className='mt-4 ml-5 text-[#00ADB5] text-[27px] pb-6 font-bold'>Register</h1>
                     <form onSubmit={submitregister}>
-                        <input onChange={(e) => {setUsername(e.target.value)}} className='mx-4 bg-[#222831] rounded-md placeholder-[#424f58] text-[#424f58] outline-none w-[300px] sm:w-[450px] pl-4 h-[40px]' placeholder='Username'/>
-                        <div className="flex bg-[#222831] items-center m-4 mt-5 rounded-md w-[300px] sm:w-[450px] pl-4 h-[40px] justify-between pr-4"><input type={visible ? "text" : "password"} onChange={(e) => {setPassword(e.target.value)}} className='w-[250px] sm:w-[400px] outline-none placeholder-[#424f58] bg-[#222831] text-[#424f58]' placeholder='Password'/><div className="cursor-pointer" onClick={() => {setVisible(!visible)}}><img className="h-6" src="/eye.svg" alt="change_visiblity" /></div></div>
-                        <div className="flex bg-[#222831] items-center m-4 mt-5 rounded-md w-[300px] sm:w-[450px] pl-4 h-[40px] justify-between pr-4"><input type={confirmvisible ? "text" : "password"} onChange={(e) => {setConfirmPassword(e.target.value)}} className='w-[250px] sm:w-[400px] outline-none placeholder-[#424f58] bg-[#222831] text-[#424f58]' placeholder='Confirm Password'/><div className="cursor-pointer" onClick={() => {setConfirmVisible(!confirmvisible)}}><img className="h-6" src="/eye.svg" alt="change_visiblity" /></div></div>
-                        <button type="submit" className="w-[300px] pl-4 h-[40px] mx-4 mt-5 rounded-md text-[#283035] font-bold bg-[#00ADB5] hover:opacity-70 duration-700 sm:w-[450px]">Register Now</button>
+                        <input onChange={(e) => {setUsername(e.target.value)}} className='mx-4 bg-[#222831] rounded-md placeholder-[#424f58] text-[#424f58] outline-none w-[87%] sm:w-[450px] pl-4 h-[40px]' placeholder='Username'/>
+                        <div className="flex bg-[#222831] items-center m-4 mt-5 rounded-md w-[87%] sm:w-[450px] pl-4 h-[40px] justify-between pr-4"><input type={visible ? "text" : "password"} onChange={(e) => {setPassword(e.target.value)}} className='w-[250px] sm:w-[400px] outline-none placeholder-[#424f58] bg-[#222831] text-[#424f58]' placeholder='Password'/><div className="cursor-pointer" onClick={() => {setVisible(!visible)}}><img className="h-6" src="/eye.svg" alt="change_visiblity" /></div></div>
+                        <div className="flex bg-[#222831] items-center m-4 mt-5 rounded-md w-[87%] sm:w-[450px] pl-4 h-[40px] justify-between pr-4"><input type={confirmvisible ? "text" : "password"} onChange={(e) => {setConfirmPassword(e.target.value)}} className='w-[250px] sm:w-[400px] outline-none placeholder-[#424f58] bg-[#222831] text-[#424f58]' placeholder='Confirm Password'/><div className="cursor-pointer" onClick={() => {setConfirmVisible(!confirmvisible)}}><img className="h-6" src="/eye.svg" alt="change_visiblity" /></div></div>
+                        <button type="submit" className="w-[87%] h-[40px] mx-4 mt-5 rounded-md text-[#283035] font-bold bg-[#00ADB5] hover:opacity-70 duration-700 sm:w-[450px]">Register Now</button>
                     </form>
                     {error && <p className="text-[#c72931] font-bold text-center text-sm">{error}</p> }           
                     {Error && <p className="text-[#c72931] font-bold text-center text-sm">{Error}</p> }           
