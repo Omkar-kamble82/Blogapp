24af35f7c8a5e883495712e4db2b2bc3663df6d0
Omkar Kamble <96938880+Omkar-kamble82@users.noreply.github.com>
2023-04-11 20:32:48 +0530

    Update Blog.tsx

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -91,7 +91,8 @@ const Blog = () => {
             })
             const json = await response.json()
             const data = JSON.stringify(json)
-            const blog = [JSON.parse(data)].map((blog:Blog) => {
+            const blog = JSON.parse(data)
+            blog = blog.map((blog:Blog) => {
                 if(blog._id === id) {
                     setUserblog(blog)
                 }
